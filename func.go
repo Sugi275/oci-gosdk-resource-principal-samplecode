cadf5056b6f02d8cee406f894c37842736698615
sugimount <sugi.mount@gmail.com>
2019-08-28 09:55:31 +0000

    first commit

---
+++ func.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"github.com/oracle/oci-go-sdk/common"
+	"context"
+	_ "encoding/json"
+	"fmt"
+	"os"
+	"io"
+
+	fdk "github.com/fnproject/fdk-go"
+	"github.com/oracle/oci-go-sdk/core"
+	"github.com/oracle/oci-go-sdk/common/auth"
+	"github.com/Sugi275/oci-objectstorage-multiregioner/loglib"
+)
+
+func main() {
+	fdk.Handle(fdk.HandlerFunc(myHandler))
+	// reader := os.Stdin
+	// writer := os.Stdout
+	// myHandler(context.TODO(), reader, writer)
+}
+
+//Person test
+type Person struct {
+	Name string `json:"name"`
+}
+
+func myHandler(ctx context.Context, in io.Reader, out io.Writer) {
+
+	provider, err := auth.ResourcePrincipalConfigurationProvider()
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	compartmentID := os.Getenv("COMPARTMENT_ID")
+
+	request := core.ListInstancesRequest{
+		CompartmentId: &compartmentID,
+		LifecycleState: core.InstanceLifecycleStateRunning,
+	}
+
+	fmt.Println(request)
+
+	client, err := core.NewComputeClientWithConfigurationProvider(provider)
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	// Override the region, this is an optional step.
+	// the InstancePrincipalsConfigurationProvider defaults to the region
+	// in which the compute instance is currently running
+	client.SetRegion(string(common.RegionAPTokyo1))
+
+	listInstancesResponse, err := client.ListInstances(context.Background(), request)
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	for _, item := range listInstancesResponse.Items {
+		fmt.Printf("list of Compute Instance: %s \n", *item.DisplayName)
+	}
+}
